3eb0dbdc73c9218b172beb6314ab6036b75a389f
Jakob Weber <jakobweber@hotmail.com>
2020-07-20 19:01:21 +0200

    update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     long_description=long_description,
